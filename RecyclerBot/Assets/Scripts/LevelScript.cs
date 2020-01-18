c8558160020c777ea6644df52cd8c963fafcd3e1
Spencer Stream <sprewstream@gmail.com>
2020-01-18 15:16:25 -0600

    Starting to use RefuseObject prefabs as objects to throw

---
+++ RecyclerBot/Assets/Scripts/LevelScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelScript : MonoBehaviour
+{
+    public GameObject[] RefuseObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
