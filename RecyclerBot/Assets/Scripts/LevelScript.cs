f171c1b655e029a1187918860c9df41a65efa6d6
Spencer Stream <sprewstream@gmail.com>
2020-01-18 16:00:26 -0600

    Added script to spawn a random refuse object upon scene start

--- RecyclerBot/Assets/Scripts/LevelScript.cs
+++ RecyclerBot/Assets/Scripts/LevelScript.cs
@@ -1,20 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class LevelScript : MonoBehaviour
 {
     public GameObject[] RefuseObjects;
+    public GameObject RefuseToLaunch;
+    public Transform Launcher;
+    public Transform SpawnPoint;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        RefuseToLaunch = GetRandomRefuseObject();
+        SpawnRefuseObjectToLaunch();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private GameObject GetRandomRefuseObject()
+    {
+        int randomObject = (int)Mathf.Floor(Random.value * RefuseObjects.Length);
+        return RefuseObjects[randomObject];
+    }
+
+    private void SpawnRefuseObjectToLaunch()
+    {
+        Instantiate(RefuseToLaunch, SpawnPoint.position, Quaternion.identity, Launcher);
     }
 }
