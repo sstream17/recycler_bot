c519d47ec994cb19604a5a4aff9f3197613a18fa
Spencer Stream <sprewstream@gmail.com>
2020-01-19 04:29:17 -0600

    Updated game to allow waiting until a shot is complete if it is in the air while the timer hits zero

--- RecyclerBot/Assets/Scripts/Score.cs
+++ RecyclerBot/Assets/Scripts/Score.cs
@@ -15,33 +15,53 @@ public class Score : MonoBehaviour
     public bool TimerFinished = false;
     public UIHandler UIHandler;
     public int CurrentLevel;
+    public GameObject Refuse;
 
     private bool levelFinished = false;
+    private bool searchingForRefuse = false;
 
     // Start is called before the first frame update
     void Start()
     {
         LevelText.text = $"Level {CurrentLevel}";
         StartCoroutine(Timer());
+        if (Refuse == null)
+        {
+            if (!searchingForRefuse)
+            {
+                searchingForRefuse = true;
+                StartCoroutine(SearchForRefuse());
+            }
+            return;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Refuse == null)
+        {
+            if (!searchingForRefuse)
+            {
+                searchingForRefuse = true;
+                StartCoroutine(SearchForRefuse());
+            }
+            return;
+        }
+
         if (TimerFinished)
         {
             if (!levelFinished)
             {
                 levelFinished = true;
-                UnityEngine.Time.timeScale = 0f;
-                UIHandler.OnLevelComplete(CurrentScore, CurrentLevel + 1);
+                StartCoroutine(WaitForBuzzerBeater(Refuse));
             }
             
         }
 
         Multiplier = Streak / 3 == 0 ? 1 : Streak / 3;
 
-        TimeText.text = FormatTime(Time);
+        TimeText.text = FormatTime();
         ScoreText.text = CurrentScore.ToString();
     }
 
@@ -61,9 +81,36 @@ public class Score : MonoBehaviour
         TimerFinished = true;
     }
 
-    private string FormatTime(float time)
+    private string FormatTime()
     {
         var timeString = $"{TimeSpan.FromSeconds(Time)}";
         return timeString.Remove(0, 3);
+    }
+
+    IEnumerator SearchForRefuse()
+    {
+        GameObject searchResult = GameObject.FindGameObjectWithTag("Refuse");
+        if (searchResult == null)
+        {
+            yield return new WaitForSeconds(1f);
+            StartCoroutine(SearchForRefuse());
+        }
+        else
+        {
+            searchingForRefuse = false;
+            Refuse = searchResult;
+            yield return false;
+        }
+    }
+
+    IEnumerator WaitForBuzzerBeater(GameObject refuse)
+    {
+        while (refuse)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        UnityEngine.Time.timeScale = 0f;
+        UIHandler.OnLevelComplete(CurrentScore, CurrentLevel + 1);
     }
 }
