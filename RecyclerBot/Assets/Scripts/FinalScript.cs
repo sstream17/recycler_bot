3e48f1b0903d7e0eb27200066ddde4a34da8bb52
Spencer Stream <sprewstream@gmail.com>
2020-01-19 09:26:35 -0600

    Adding final touches :)

---
+++ RecyclerBot/Assets/Scripts/FinalScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalScript : MonoBehaviour
+{
+    public ThrowBall r3;
+    public Animator Animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (r3.WasLaunched)
+        {
+            StartCoroutine(WaitToEnd());
+        }
+    }
+
+    IEnumerator WaitToEnd()
+    {
+        yield return new WaitForSeconds(2f);
+        Animator.SetTrigger("Flare");
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("The_End");
+    }
+}
--- RecyclerBot/Assets/Scripts/FinallyHandler.cs
+++ RecyclerBot/Assets/Scripts/FinallyHandler.cs
@@ -26,7 +26,7 @@ public class FinallyHandler : MonoBehaviour
         string[] text = { "R3 has officially recycled and cleaned up all the waste humans left behind. Humans have returned and learned how valuable and necessary recycling truly is.",
             "They overthrew the big corporations and started teaching kids in school about green initiatives. But with all this said and done, what is left for our R3 to do?",
             "Well R3 you did it! What will you do with all your free time now that there is nothing else to recycle?",
-            "For one last selfless act for humanity. R3 has decided to recycle itself. Thank you for everything r3!"
+            "For one last selfless act for humanity..."
         };
         dialogueManager.setText(text, "Narrator");
 
@@ -36,6 +36,6 @@ public class FinallyHandler : MonoBehaviour
 
     public void loadNextLevel()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene("Ultimate_Scene");
     }
 }
