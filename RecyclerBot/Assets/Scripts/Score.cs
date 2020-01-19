51a60b3f2ebf77593a9c1f59774680dd0831dc30
Spencer Stream <sprewstream@gmail.com>
2020-01-18 23:55:15 -0600

    Added Score script. Currently handles game timer

---
+++ RecyclerBot/Assets/Scripts/Score.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Collections;
+using System;
+
+public class Score : MonoBehaviour
+{
+    public TextMeshProUGUI ScoreText;
+    public TextMeshProUGUI TimeText;
+    public float Time = 60f;
+    public bool TimerFinished = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Timer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TimerFinished)
+        {
+            // Disable input
+
+            // Pause
+            UnityEngine.Time.timeScale = 0f;
+
+            // Show menu
+
+        }
+
+        TimeText.text = FormatTime(Time);
+    }
+
+    IEnumerator Timer()
+    {
+        while (Time > 0f)
+        {
+            yield return new WaitForSeconds(1f);
+            Time--;
+        }
+
+        TimerFinished = true;
+    }
+
+    private string FormatTime(float time)
+    {
+        var timeString = $"{TimeSpan.FromSeconds(Time)}";
+        return timeString.Remove(0, 3);
+    }
+}
